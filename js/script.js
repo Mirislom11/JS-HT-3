9dd9cf1a3f5f31483915af2ab0e953a0e0c3d038
Mirislom <18@.ru>
2021-03-24 22:40:46 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,80 @@
+
+// Задание (База пользователя)
+// создайте пустой объект , цикл от 1 до 10 и при помощи prompt  вводятся 10 чедовек имя и возраст 
+// Итерация это Id пользователя Id не  может быть 0 
+// В эту итеррацию запичывается имени и возраста 
+
+// Потом сделать вывод в console например 
+// Пользователь 3
+// Имя Иван 
+// Возраст 26
+// Пользователь 4 
+// Имя Елена 
+// возраст 26
+
+// После вывести объект а консоль
+
+// let users = {
+//  }
+
+//  for(let i=1; i<=10; i++){
+
+//     users[i] = {
+//        name: prompt("Введите имя"),
+//       age: +prompt("Введите год рождения")
+//     }
+    
+//     console.log("номер пользователя" + i);
+//     console.log(users[i]['name']);
+//     console.log(users[i]['age']);
+
+// }
+
+// console.log(users);
+
+
+// *********************************
+
+
+
+// let cart = receipt();
+// console.log("Вы заказали:");
+// let sum = 0;
+// let info
+// // console.log(cart);
+
+// for (var key in cart) {
+//     console.log(key);
+//     info = cart[key].info; 
+//     console.log(info);
+//     sum += cart[key].price
+// }
+// console.log("общая сумма = " + sum + " с доставкой " + (9000 + sum));
+
+
+
+// ************************************
+
+// let arr=[];
+
+
+//   for(let i = 0; i<5; i++) {
+//      let command = prompt("введите команду") ;
+//      let addArr = command.split(',');
+  
+//      if(addArr[0]=="add"){
+//         arr.push(addArr[1]);
+         
+//      }else if (addArr[0]=="del"){
+//          for(let j=0; j<arr.length; j++){
+//              arr[0]==addArr[1] ? arr.splice(j, 1) : '';
+//          }
+//      }else if (addArr[0]=="stop"){
+//          break;
+//      }
+//      console.log(arr);
+//   }
+
+
+
+
